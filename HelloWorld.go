1b07b87e1c498f7f0ba8e78c6f6806d43c469eb1
hiennm <nguyenminhhien@gmail.com>
2019-07-19 15:37:57 +0700

    Update HelloWorld.go

--- HelloWorld.go
+++ HelloWorld.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Make change for staging demo")
 	fmt.Println("Adding 1 more line to demo in Code")
+	fmt.Println("Adding another line to demo pull cmd")
 }
