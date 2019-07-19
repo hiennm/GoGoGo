e24ad235a1ba6ea30f493907a8fa51173799973c
Nguyen Minh Hien <nguyenminhhien@gmail.com>
2019-07-19 16:23:23 +0700

    changed HelloWorld.go

--- HelloWorld.go
+++ HelloWorld.go
@@ -7,4 +7,6 @@ func main() {
 	fmt.Println("Make change for staging demo")
 	fmt.Println("Adding 1 more line to demo in Code")
 	fmt.Println("Adding another line to demo pull cmd")
+	fmt.Println("This line belongs to my-branch")
+	
 }
